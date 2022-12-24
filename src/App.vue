e3eb6d29cb4087777d1a74d33fb79dbbe57efc8e
zl <911423305@qq.com>
2022-12-24 19:05:10 +0800

    缓存

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view />
+    <keep-alive>
+      <router-view v-if="$route.meta.path" />
+    </keep-alive>
+    <router-view v-if="!$route.meta.path" />
   </div>
 </template>
 
